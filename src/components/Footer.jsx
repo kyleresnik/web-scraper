b885c495d1487b5e03f8ad68ecfb03bd31029435
kresnik <kresnik.dev@gmail.com>
2025-02-14 23:05:08 -0500

    styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-blue-500 w-full py-4 text-white text-center">
+      <p>2024 Airbnb Web Scraper</p>
+    </footer>
+  );
+}
+
+export default Footer;
